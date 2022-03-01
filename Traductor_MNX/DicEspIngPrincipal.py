09b80b032749ccd27c910201a803cc5b6e82e145
xime-ngrn <xmorenon2000@alumno.ipn.mx>
2022-03-01 15:15:04 -0600

    verificando si la palabra ya se encuentra en la lista

--- Traductor_MNX/DicEspIngPrincipal.py
+++ Traductor_MNX/DicEspIngPrincipal.py
@@ -1,5 +1,6 @@
 cantPalabras=0
 diccionario={}
+repeticiones=[]
 valores=[]
 valores1=[]
 valores2=[]
@@ -13,40 +14,44 @@ cantPalabras=int(input("Introduce la cantidad de palabras que deseas agregar: ")
 for i in range(cantPalabras):
     print("---------------------------------------")
     llave=input("Introduce tu palabra en español: ")
-    cantAsociaciones = int(input("Introduce la cantidad de palabras en inglés deseadas: "))
-
-    if (cantAsociaciones > 1):
-        if(asociacionesMultiples==0):
-            for j in range(cantAsociaciones):
-                value = input("Introduce una asociación en inglés: ")
-                valores.insert(j, value)
-            diccionario[llave] = valores
-            asociacionesMultiples += 1
-            continue
-        if(asociacionesMultiples==1):
-            for j in range(cantAsociaciones):
-                value = input("Introduce una asociación en inglés: ")
-                valores1.insert(j, value)
-            diccionario[llave] = valores1
-            asociacionesMultiples += 1
-            continue
-        if(asociacionesMultiples==2):
-            for j in range(cantAsociaciones):
-                value = input("Introduce una asociación en inglés: ")
-                valores2.insert(j, value)
-            diccionario[llave] = valores2
-            asociacionesMultiples += 1
-            continue
-        if (asociacionesMultiples == 3):
-            for j in range(cantAsociaciones):
-                value = input("Introduce una asociación en inglés: ")
-                valores3.insert(j, value)
-            diccionario[llave] = valores3
-            asociacionesMultiples += 1
-            continue
+    if(llave in repeticiones):
+        print("¡ERROR!")
     else:
-        value = input("Introduce una asociación en inglés: ")
-        diccionario[llave] = value
+        repeticiones.append(llave)
+        cantAsociaciones = int(input("Introduce la cantidad de palabras en inglés deseadas: "))
+
+        if (cantAsociaciones > 1):
+            if(asociacionesMultiples==0):
+                for j in range(cantAsociaciones):
+                    value = input("Introduce una asociación en inglés: ")
+                    valores.insert(j, value)
+                diccionario[llave] = valores
+                asociacionesMultiples += 1
+                continue
+            if(asociacionesMultiples==1):
+                for j in range(cantAsociaciones):
+                    value = input("Introduce una asociación en inglés: ")
+                    valores1.insert(j, value)
+                diccionario[llave] = valores1
+                asociacionesMultiples += 1
+                continue
+            if(asociacionesMultiples==2):
+                for j in range(cantAsociaciones):
+                    value = input("Introduce una asociación en inglés: ")
+                    valores2.insert(j, value)
+                diccionario[llave] = valores2
+                asociacionesMultiples += 1
+                continue
+            if (asociacionesMultiples == 3):
+                for j in range(cantAsociaciones):
+                    value = input("Introduce una asociación en inglés: ")
+                    valores3.insert(j, value)
+                diccionario[llave] = valores3
+                asociacionesMultiples += 1
+                continue
+        else:
+            value = input("Introduce una asociación en inglés: ")
+            diccionario[llave] = value
 
 print("*******************")
 print(diccionario)
