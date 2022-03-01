66b4505bf93d10b5f0031afda1f7771e42eddef8
xime-ngrn <xmorenon2000@alumno.ipn.mx>
2022-03-01 11:38:16 -0600

    2do intento no sirve list

--- Traductor_MNX/DicEspIng.py
+++ Traductor_MNX/DicEspIng.py
@@ -11,11 +11,18 @@ for i in range(cantPalabras):
     key=input("Introduce una palabra en español: ")
     cantAsociaciones=int(input("Introduce la cantidad de palabras en inglés deseadas: "))
 
-    for j in range(cantAsociaciones):
+    if(cantAsociaciones==1):
         value=input("Introduce una palabra asociada: ")
-        diccionario[key] = value
-        #solo guarda un valor
-        #si utilizo una lista, todas las llaves tienen todos los valores de la lista
+        diccionario[key]=value
+    else:
+        for j in range(cantAsociaciones):
+            value = input("Introduce una palabra asociada: ")
+            palabrasIngles.insert(j, value)
+            if(j==cantAsociaciones):
+                diccionario[key] = palabrasIngles
+
+#NO ESTA GUARDANDO LA TUPLA :(
+    #si utilizo una lista, todas las llaves tienen todos los valores de la lista
 
     print("-----------")
     print(diccionario)
