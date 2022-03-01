5e16f6def5ac4d146b847e12ce19296882d6c12d
xime-ngrn <xmorenon2000@alumno.ipn.mx>
2022-03-01 13:06:29 -0600

    6to intento con puro list

--- Traductor_MNX/DiccionarioEspIng.py
+++ Traductor_MNX/DiccionarioEspIng.py
@@ -1,6 +1,6 @@
 cantPalabras=0
 diccionario={}
-llaves=[]
+#llaves=[]
 valores=[]
 
 print("*****Bienvenido*****")
@@ -8,21 +8,22 @@ print("Diccionario de Español-Inglés")
 cantPalabras=int(input("Introduce la cantidad de palabras que deseas agregar: "))
 
 for i in range(cantPalabras):
-    pEspañol=input("Introduce tu palabra en español: ")
-    llaves.insert(pEspañol)
+    llave=input("Introduce tu palabra en español: ")
+    #pEspañol=input("Introduce tu palabra en español: ")
+    #llaves.append(pEspañol)
     cantAsociaciones = int(input("Introduce la cantidad de palabras en inglés deseadas: "))
 
     if(cantAsociaciones>1):
         for j in range(cantAsociaciones):
+            global valores
             value = input("Introduce una asociación en inglés: ")
             valores.insert(j, value)
-        diccionario[llaves] = valores
-        #no guarda nada en el diccionario
+            print("||||| Terminó de agregar el valor, wiiii")
+        diccionario[llave] = valores
     else:
         value = input("Introduce una asociación en inglés: ")
-        diccionario[llaves]=value
+        diccionario[llave]=value
 
-print("*******************")
 print("*******************")
 print(diccionario)
 print("*******************")
